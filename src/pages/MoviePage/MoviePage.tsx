41034694a99ae6d62dbd59a5ad6d746e0ad039d2
Sushmit Rajaure <39255317+Sushmit1997@users.noreply.github.com>
2021-08-21 10:27:24 +0545

    Unused imports removed

--- src/pages/MoviePage/MoviePage.tsx
+++ src/pages/MoviePage/MoviePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import MovieCard from '../../components/MovieCard/MovieCard'
 import movieData from '../../utils/dummyData'
 import CircularProgress from '@material-ui/core/CircularProgress'
